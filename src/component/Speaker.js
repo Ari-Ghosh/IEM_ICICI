95e5b9fc7960c38e861d8b1efb1b7c4641fc661e
drghosh90 <101732943+drghosh90@users.noreply.github.com>
2023-07-21 08:22:47 +0000

    ok

--- src/component/Speaker.js
+++ src/component/Speaker.js
@@ -7,7 +7,54 @@ function Speaker() {
     <div className='hero_Speaker'>
       <h1 className='heading_Speaker'>TO BE ANNOUNCED SOON</h1>
       <br></br>
-      
+      <h2 className='role_Speaker'>KEYNOTE SPEAKERS</h2>
+      <br></br>
+      <div className="container_speaker">
+        <div className="placeholder">
+          <div className='pic_keynote'>
+            <img className="profile" src={Profile} alt="" />
+          </div>
+          <h3> <br></br> </h3>
+        </div>
+        <br></br>
+        <div className="placeholder">
+          <div className='pic_keynote'>
+            <img className="profile" src={Profile} alt="" />
+          </div>
+          <h3> <br></br> </h3>
+        </div>
+      </div>
+      <br></br>
+      <h2 className='role_Speaker'>SESSION CHAIRS</h2>
+      <br></br>
+      <div className="container_speaker4">
+        <div className="placeholder">
+          <div className='pic_session'>
+            <img className="profile" src={Profile} alt="" />
+          </div>
+          <h3> <br></br> </h3>
+        </div>
+        <br></br>
+        <div className="placeholder">
+          <div className='pic_session'>
+            <img className="profile" src={Profile} alt="" />
+          </div>
+          <h3> <br></br> </h3>
+        </div>
+        <div className="placeholder">
+          <div className='pic_session'>
+            <img className="profile" src={Profile} alt="" />
+          </div>
+          <h3> <br></br> </h3>
+        </div>
+        <br></br>
+        <div className="placeholder">
+          <div className='pic_session'>
+            <img className="profile" src={Profile} alt="" />
+          </div>
+          <h3> <br></br> </h3>
+        </div>
+      </div>
     </div>
   )
 }
