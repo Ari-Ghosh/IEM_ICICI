942a12b85cd514d3714bc960df56aa19ac97df2c
drghosh90 <101732943+drghosh90@users.noreply.github.com>
2023-07-21 07:58:44 +0000

    speaker update

--- src/component/Speaker.js
+++ src/component/Speaker.js
@@ -6,7 +6,7 @@ function Speaker() {
   return (
     <div className='hero_Speaker'>
       <h1 className='heading_Speaker'>List of Speakers</h1>
-      <h3 className='role_Speaker'>To be announced soon</h3>
+      <h2 className='role_Speaker'>To be announced soon</h2>
       </div>
 )
 }
