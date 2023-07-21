d9b3e854454d5997a21d7bd3462ce25a94974370
drghosh90 <101732943+drghosh90@users.noreply.github.com>
2023-07-21 07:57:16 +0000

    speaker update

--- src/component/Speaker.js
+++ src/component/Speaker.js
@@ -4,9 +4,9 @@ import Profile from '../assets/profile.jpg'
 
 function Speaker() {
   return (
-    <div className='hero_Submit'>
-      <h1 className='heading_Submit'>List of Speakers</h1>
-      <h3 className='para_Submit'>To be announced soon</h3>
+    <div className='hero_Speaker'>
+      <h1 className='heading_Speaker'>List of Speakers</h1>
+      <h3 className='role_Speaker'>To be announced soon</h3>
       </div>
 )
 }
